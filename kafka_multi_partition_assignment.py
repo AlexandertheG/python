3f5b0ad37ddc6b395f3018c857497b8fed53f928
AlexandertheG <al3xag295@gmail.com>
2023-09-09 14:30:52 -0700

    Create kafka_multi_partition_assignment.py
    Add kafka_multi_partition_assignment.py

---
+++ kafka_multi_partition_assignment.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import json
+
+idx = 0
+arr = []
+result = {"version":1, "partitions":[]}
+repl_set = {1, 2, 3}
+with open("iti.content.events.txt") as fd:
+    lines = fd.readlines()
+    for ln in lines:
+        ln = ln.rstrip()
+        if ln:
+            repl_num = int(ln.split("Isr: ")[1])
+            curr_set = {repl_num}
+            new_repl_set = repl_set - curr_set
+            new_repl_arr = [repl_num]
+            new_repl_arr.extend(list(new_repl_set))
+            tmp_dict = {"topic":"iti.content.events", "partition":idx, "replicas": new_repl_arr}
+            result["partitions"].append(tmp_dict)
+            arr.append(tmp_dict)
+            idx+=1
+
+print(json.dumps(result))
